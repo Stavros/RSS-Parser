253bd3616963a7db52f788e062ae132b53d7f094
Stavros <stavros@ubinet.gr>
2018-10-25 21:13:46 +0200

    add custom url feature

---
+++ ArduinoGRfeeder/src/main/java/com/arduinogr/feeder/MainActivity.java
@@ -0,0 +1,241 @@
+/*
+*   Copyright 2018 Stavros Kalapothas
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+*/
+
+package com.arduinogr.feeder;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.prof.rssparser.Article;
+import com.prof.rssparser.Parser;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.widget.EditText;
+import android.util.Patterns;
+
+/**
+ * Created by Stavros Kalapothas
+ * based on RSS Parser library.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ArticleAdapter mAdapter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private ProgressBar progressBar;
+    private String urlString = "http://www.arduinogr.com/feeds/posts/default?alt=rss";
+    private String url_text = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        mRecyclerView = findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setHasFixedSize(true);
+
+        mSwipeRefreshLayout = findViewById(R.id.container);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary, R.color.colorPrimaryDark);
+        mSwipeRefreshLayout.canChildScrollUp();
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+
+                mAdapter.clearData();
+                mAdapter.notifyDataSetChanged();
+                mSwipeRefreshLayout.setRefreshing(true);
+                loadFeed();
+            }
+        });
+
+        if (!isNetworkAvailable()) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.alert_message)
+                    .setTitle(R.string.alert_title)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.alert_positive,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    finish();
+                                }
+                            });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+
+        } else if (isNetworkAvailable()) {
+            loadFeed();
+        }
+    }
+
+    public void loadFeed() {
+
+        if (!mSwipeRefreshLayout.isRefreshing())
+            progressBar.setVisibility(View.VISIBLE);
+
+        Parser parser = new Parser();
+        parser.execute(urlString);
+        parser.onFinish(new Parser.OnTaskCompleted() {
+            //what to do when the parsing is done
+            @Override
+            public void onTaskCompleted(ArrayList<Article> list) {
+                //list is an Array List with all article's information
+                //set the adapter to recycler view
+                mAdapter = new ArticleAdapter(list, R.layout.row, MainActivity.this);
+                mRecyclerView.setAdapter(mAdapter);
+                progressBar.setVisibility(View.GONE);
+                mSwipeRefreshLayout.setRefreshing(false);
+
+            }
+
+            //what to do in case of error
+            @Override
+            public void onError() {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        Toast.makeText(MainActivity.this, "Unable to load data.",
+                                Toast.LENGTH_LONG).show();
+                        Log.i("Unable to load ", "articles");
+                    }
+                });
+            }
+        });
+    }
+
+    public boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+        if (mAdapter != null)
+            mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+        if (mAdapter != null)
+            mAdapter.clearData();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private boolean isValidUrl(String url) {
+        Pattern p = Patterns.WEB_URL;
+        Matcher m = p.matcher(url.toLowerCase());
+        if(m.matches())
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_about) {
+            android.support.v7.app.AlertDialog alertDialog = new android.support.v7.app.AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle(R.string.app_name);
+            alertDialog.setMessage(Html.fromHtml(MainActivity.this.getString(R.string.info_text) +
+                    " <a href='http://github.com/Stavros/RSS-Parser'>GitHub</a>" +
+                    MainActivity.this.getString(R.string.author)));
+            alertDialog.setButton(android.support.v7.app.AlertDialog.BUTTON_NEUTRAL, "Close",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+
+            ((TextView) alertDialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        if (id == R.id.action_url) {
+            android.support.v7.app.AlertDialog alertDialog = new android.support.v7.app.AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle(R.string.url_feed);
+            alertDialog.setMessage("Enter URL (include http://)");
+            final EditText input = new EditText(this);
+            alertDialog.setView(input);
+            alertDialog.setButton(android.support.v7.app.AlertDialog.BUTTON_NEUTRAL, "Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            url_text = input.getText().toString();
+                            if (isValidUrl(url_text)) {
+                                urlString = url_text;
+                                return;
+                            }
+                            urlString = "http://www.arduinogr.com/feeds/posts/default?alt=rss";
+                        }
+                    });
+            alertDialog.show();
+
+            ((TextView) alertDialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
